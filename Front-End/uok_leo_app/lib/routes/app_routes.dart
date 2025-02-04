28b86d3da6e6e0620bc16bfc45118bea57fad6b2
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-02-04 23:34:41 +0530

    app routes added

--- Front-End/uok_leo_app/lib/routes/app_routes.dart
+++ Front-End/uok_leo_app/lib/routes/app_routes.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:uok_leo_app/screens/home_screen.dart';
 import 'package:uok_leo_app/screens/login_screen.dart';
-import 'package:uok_leo_app/screens/profile_screen.dart';
 
 class AppRoutes {
   static Map<String, Widget Function(BuildContext)> routes = {
     '/login': (context) => const LoginScreen(),
+    '/home': (context) => const HomeScreen(),
   };
 }
